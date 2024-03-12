4ee5ff2d7c22bccd147aea4ab1a01ad5150680b0
Nurkhissa <45072957+Nurkhissa@users.noreply.github.com>
2024-03-12 21:51:45 +0500

    AddUser tayyor

--- UserManagment.Console/Brokers/StorageBroker.cs
+++ UserManagment.Console/Brokers/StorageBroker.cs
@@ -1,13 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using UserManagment.Models;
 
 namespace UserManagment.Brokers
 {
-    internal class StorageBroker
+    internal class StorageBroker : IStorageBroker
     {
+        public static string FilePath = "~/../../../../Assets/Data.txt";
 
+        public StorageBroker() 
+        {
+            EnsureFileExsists();
+        }
+        public User AddUser(User user)
+        {
+            string userLine = $"{user.UserName} - {user.Password}";
+            
+            File.AppendAllText(FilePath, userLine);
+
+            return user;
+
+        }
+
+        public void EnsureFileExsists()
+        {
+            if (!File.Exists(FilePath))
+            {
+                File.Create(FilePath);
+            }
+        }
     }
 }
